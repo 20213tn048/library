f31d9ff9a567a86bf8cb6f168e1fd2e8a4f0dfaa
20213tn048 <20213tn048@utez.edu.mx>
2024-02-15 12:57:09 -0600

    fix: Change structure of components
    Create new components to make easier the payload.

--- frontend/src/components/BookCards.vue
+++ frontend/src/components/BookCards.vue
@@ -1,9 +1,116 @@
 <template>
-    
+    <div>
+        <div v-if="loading" class="loader">Cargando...</div>
+        <div v-if="!loading">
+            <b-container fluid>
+                <b-card-group deck>
+                    <b-row>
+                        <b-col cols="12" md="6" lg="4" v-for="(book, index) in books.data" :key="index" class="mb-4">
+                            <b-card :image-src="book.photo" img-alt="book.name" img-top :title="book.name" class="mt-2">
+                                <b-card-text>
+                                    {{ book.author }}
+                                </b-card-text>
+                                <template #footer>
+                                    <small class="text-muted">Año de publicación: {{ book.publicationYear }}</small>
+                                </template>
+                                <b-dropdown variant="primary" text="Opciones" class="mt-2">
+                                    <b-dropdown-item @click="openUpdateModal">Actualizar</b-dropdown-item>
+                                    <b-dropdown-item @click="openDeleteModal(book.id)">Borrar</b-dropdown-item>
+                                </b-dropdown>
+                            </b-card>
+                        </b-col>
+                    </b-row>
+                </b-card-group>
+            </b-container>
+        </div>
+
+        <!-- Modal de Actualizar -->
+        <b-modal v-model="showUpdateModal" title="Actualizar Libro">
+            <!-- Contenido del modal de Actualizar -->
+            <!-- Aquí puedes agregar el formulario o contenido que desees para la actualización -->
+            <UpdateBook/>
+        </b-modal>
+
+        <!-- Modal de Eliminar -->
+        <b-modal v-model="showDeleteModal" title="Eliminar Libro" hide-footer hide-header centered>
+            <h2 class="mt-2 text-center">¿Estás seguro de borrar el libro?</h2>
+            <p class="mt-4 text-center">Los datos se eliminarán permanentemente.</p>
+            <div class="mt-4 d-flex justify-content-center">
+                <b-button variant="danger" class="mr-2" @click="deleteBook">Si, Borrar</b-button>
+                <b-button variant="secondary" class="ml-2" @click="closeDeleteModal">Cancelar</b-button>
+            </div>
+        </b-modal>
+    </div>
 </template>
 
 <script>
+import getBooks from '../services/GetBooks.js';
+import showMessageSuccess from '../services/Alerts';
+import DeleteBook from '../services/DeleteBook';
+import UpdateBook from './UpdateBook.vue';
 
+export default {
+    data() {
+        return {
+            loading: true,
+            books: [],
+            showUpdateModal: false,
+            showDeleteModal: false,
+            bookIdToDelete: null
+        }
+    },
+    methods: {
+        async getBook() {
+            try {
+                this.books = await getBooks();
+                showMessageSuccess("Carga completa de datos", "success");
+            } catch (error) {
+                showMessageSuccess("Error al obtener la información del servidor", "error");
+            } finally {
+                this.loading = false;
+            }
+        },
+        openUpdateModal() {
+            this.showUpdateModal = true;
+        },
+        openDeleteModal(id) {
+            this.showDeleteModal = true;
+            this.bookIdToDelete = id;
+        },
+        closeDeleteModal() {
+            this.showDeleteModal = false;
+        },
+        async deleteBook() {
+            try {
+                await DeleteBook(this.bookIdToDelete);
+                showMessageSuccess("Libro eliminado exitosamente", "success");
+                // Actualizar la lista de libros u otras acciones necesarias después de eliminar
+                this.getBook();
+            } catch (error) {
+                showMessageSuccess("Error al eliminar el libro", "error");
+            } finally {
+                this.closeDeleteModal();
+            }
+        }
+    },
+    mounted() {
+        this.getBook();
+    },
+    components: {
+        UpdateBook
+    }
+}
 </script>
 
-<style></style>
+<style scoped>
+.loader {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+} 
+</style>
--- frontend/src/components/BookForm.vue
+++ frontend/src/components/BookForm.vue
@@ -1,29 +1,41 @@
 <template>
-  <b-form v-if="show" @submit="addNewBook">
+  <b-form v-if="show" @submit.prevent="submitForm">
     <b-form-group
+      :state="nameState"
       label="Titulo de la obra"
+      :class="{ 'is-invalid': formSubmitted && !nameState }"
+      invalid-feedback="El título es requerido"
     >
       <b-form-input v-model="name"></b-form-input>
     </b-form-group>
     <b-form-group
+      :state="authorState"
       label="Autor"
+      :class="{ 'is-invalid': formSubmitted && !authorState }"
+      invalid-feedback="El autor es requerido"
     >
       <b-form-input v-model="author"></b-form-input>
     </b-form-group>
     <b-form-group
+      :state="publicationYearState"
       label="Año de publicación"
+      :class="{ 'is-invalid': formSubmitted && !publicationYearState }"
+      invalid-feedback="El año de publicación debe ser menor o igual al año actual"
     >
       <b-form-input v-model="publicationYear"></b-form-input>
     </b-form-group>
     <b-form-group
+      :state="genreState"
       label="Género"
+      :class="{ 'is-invalid': formSubmitted && !genreState }"
+      invalid-feedback="El género es requerido"
     >
       <b-form-select v-model="selectedGenre">
         <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.description }}</option>
       </b-form-select>
     </b-form-group>
     <b-button type="submit" class="mt-3" variant="primary">Registrar nuevo libro</b-button>
-  <div v-if="loading" class="loader">Cargando...</div>
+    <div v-if="loading" class="loader">Cargando...</div>
   </b-form>
 </template>
 
@@ -39,10 +51,25 @@ export default {
       author: null,
       publicationYear: null,
       genres: [],
-      selectedGenre: null, // Agrega una propiedad para almacenar el género seleccionado
+      selectedGenre: null,
       show: true,
-      loading: false
+      loading: false,
+      formSubmitted: false
     };
+  },
+  computed: {
+    nameState() {
+      return this.name ? true : false;
+    },
+    authorState() {
+      return this.author ? true : false;
+    },
+    publicationYearState() {
+      return this.publicationYear && parseInt(this.publicationYear) <= new Date().getFullYear() ? true : false;
+    },
+    genreState() {
+      return this.selectedGenre ? true : false;
+    }
   },
   methods: {
     async getGenres() {
@@ -53,22 +80,32 @@ export default {
         showMessageSuccess("Error al obtener la información del servidor", "error");
       } 
     },
-    async addNewBook(event){
-      event.preventDefault()
+    async submitForm() {
+      this.formSubmitted = true;
 
-      try{
-        this.loading = true
-  
-        const message = await addNewBook(this.name, this.author,this.publicationYear,this.selectedGenre)
-        console.log(message)
-        if(message.status === 201){
-          showMessageSuccess("Libro creado exitosamente", "success").then(()=> window.location.reload())
+      if (!this.name || !this.author || !this.publicationYear || !this.selectedGenre) {
+        showMessageSuccess("Todos los campos son requeridos", "error");
+        return;
+      }
+
+      if (parseInt(this.publicationYear) > new Date().getFullYear()) {
+        showMessageSuccess("El año de publicación no puede ser mayor al año actual", "error");
+        return;
+      }
+
+      try {
+        this.loading = true;
+
+        const message = await addNewBook(this.name, this.author, this.publicationYear, this.selectedGenre);
+        console.log(message);
+        if (message.status === 201) {
+          showMessageSuccess("Libro creado exitosamente", "success").then(() => window.location.reload());
         }
-      }catch(error){
-        showMessageSuccess("Error al registrar nuevo producto", "error")
-        console.error(error)
-      }finally{
-        this.loading = false
+      } catch (error) {
+        showMessageSuccess("Error al registrar nuevo producto", "error");
+        console.error(error);
+      } finally {
+        this.loading = false;
       }
     }
   },
@@ -79,15 +116,20 @@ export default {
 </script>
 
 <style scoped>
+.is-invalid .form-control,
+.is-invalid .form-control-plaintext,
+.is-invalid .custom-select,
+.is-invalid .form-check-input {
+  border-color: #dc3545;
+}
 
-.loader {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgba(255, 255, 255, 0.8);
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+.is-invalid .form-check-input:indeterminate ~ .custom-control-label::before,
+.is-invalid .form-check-input:checked ~ .custom-control-label::before {
+  border-color: #dc3545;
+  background-color: #dc3545;
+}
+
+.is-invalid .form-check-input:focus ~ .custom-control-label::before {
+  box-shadow: 0 0 0 1px #fff, 0 0 0 0.2rem rgba(220, 53, 69, 0.25);
 }
 </style>
